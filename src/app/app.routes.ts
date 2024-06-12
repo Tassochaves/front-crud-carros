daaad02f0ed11b54de6bf8ffe68e07c45617236b
Tassochaves <tassogt@hotmail.com>
2024-06-11 23:09:53 -0300

    primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CarroListComponent } from './components/carro-list/carro-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", redirectTo: "dashboard", pathMatch: 'full'},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "carros", component: CarroListComponent}
+];
