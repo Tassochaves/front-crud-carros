aa650c39eafe7195c131ff7f2d9f9a9d8bc862ed
Tassochaves <tassogt@hotmail.com>
2024-06-13 16:40:13 -0300

    Add tela de login, listagem e cadastro carros

--- src/app/components/carros/carrosdetails/carrosdetails.component.ts
+++ src/app/components/carros/carrosdetails/carrosdetails.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Carro } from '../../../models/carro';
 
 @Component({
   selector: 'app-carrosdetails',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, ReactiveFormsModule, FormsModule],
   templateUrl: './carrosdetails.component.html',
   styleUrl: './carrosdetails.component.scss'
 })
 export class CarrosdetailsComponent {
 
+  carro: Carro = new Carro(0, "");
+
+  salvar(){
+
+  }
 }
--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  usuario!: string;
+  senha!: string;
+
+  router = inject(Router);
+
+  logar(){
+    if(this.usuario == 'admin' && this.senha == 'admin'){
+      this.router.navigate(['admin/carros']);
+    }else{
+      alert('Usuário ou senha invalida!')
+    }
+  }
 
 }
