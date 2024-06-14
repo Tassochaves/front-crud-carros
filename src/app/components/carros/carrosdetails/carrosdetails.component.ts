d44015650e2cef2c9b839e4ecd87852e2f7811ae
Tassochaves <tassogt@hotmail.com>
2024-06-13 23:07:27 -0300

    Add popup sweetalert2

--- src/app/components/carros/carrosdetails/carrosdetails.component.ts
+++ src/app/components/carros/carrosdetails/carrosdetails.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Carro } from '../../../models/carro';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carrosdetails',
@@ -14,7 +16,39 @@ export class CarrosdetailsComponent {
 
   carro: Carro = new Carro(0, "");
 
-  salvar(){
+  router = inject(ActivatedRoute);
+  routeRedirect = inject(Router);
 
+  constructor(){
+
+    let id = this.router.snapshot.params['id'];
+    if(id > 0){
+      this.findById(id);
+    }
+  }
+
+  findById(id: number){
+    let carroRetornado: Carro = new Carro(id, "Hilux");
+    this.carro = carroRetornado;
+  }
+
+  salvar(){
+    if(this.carro.id > 0){
+      Swal.fire({
+        title: 'Editado com sucesso!',
+        icon: 'success',
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: 'Ok'
+      });
+      this.routeRedirect.navigate(['admin/carros'], {state: {carroEditado: this.carro}});
+    }else{
+      Swal.fire({
+        title: 'Salvo com sucesso!',
+        icon: 'success',
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: 'Ok'
+      });
+      this.routeRedirect.navigate(['admin/carros'], {state: {carroNovo: this.carro}});
+    }
   }
 }
--- src/app/components/carros/carroslist/carroslist.component.ts
+++ src/app/components/carros/carroslist/carroslist.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Carro } from '../../../models/carro';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carroslist',
@@ -14,13 +15,48 @@ export class CarroslistComponent {
   lista: Carro[] = [];
 
   constructor(){
+
     this.lista.push(new Carro(1, "Fiesta"));
     this.lista.push(new Carro(2, "Uno"));
     this.lista.push(new Carro(3, "Hilux"));
     this.lista.push(new Carro(4, "S10"));
+
+    let carroNovo = history.state.carroNovo;
+    let carroEditado = history.state.carroEditado;
+
+    if(carroNovo){
+      carroNovo.id = 12;
+      this.lista.push(carroNovo);
+    }
+
+    if(carroEditado){
+      let indice = this.lista.findIndex(x => {return x.id == carroEditado.id});
+      this.lista[indice] = carroEditado;
+    }
   }
 
-  deletar(){
+  deleteById(carro: Carro){
+    Swal.fire({
+      title: 'Tem certeza que deseja deletar este registro?',
+      icon: 'warning',
+      showConfirmButton: true,
+      showDenyButton: true,
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não'
+    }).then((result) => {
+      if(result.isConfirmed){
+        let indice = this.lista.findIndex(x => {return x.id == carro.id});
+        this.lista.splice(indice, 1);
+
+        Swal.fire({
+          title: 'Deletado com sucesso!',
+          icon: 'success',
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: 'Ok'
+        });
+      }
+    });
 
   }
 }
